24da612ae330c8b36b8f9afec2d791c511c47c00
PARDO Christopher <Christopher.PARDO@cpnv.ch>
2019-03-08 09:33:45 +0100

    première partide de la grille avec les bords

--- main.c
+++ main.c
@@ -4,10 +4,33 @@
  * 03.03.2019
  */
 #include <stdio.h>
+#include <windows.h>
+#define STLC 218 // ┌, Single Top Left Corner
+#define STRC 191 // ┐, Single Top Right Corner
+#define SBLC 192 // └, Single Bottom Left Corner
+#define SBRC 217 // ┘, Single Bottom Right Corner
+#define SVSB 179 // │, Single Vertical Simple Border
+#define SVRB 180 // ┤, Single Vertical Right Border
+#define SVLB 195 // ├, Single Vertical Left Border
+#define SHSB 196 // ─, Single Horizontal Simple Border
+#define SHBB 193 // ┴, Single Horizontal Bottom Border
+#define SHTB 194 // ┬, Single Horizontal Top Border
+#define SC   197 // ┼, Single Center
+#define DTLC 201 // ╔, Double Top Left Corner
+#define DTRC 187 // ╗, Double Top Right Corner
+#define DBLC 200 // ╚, Double Bottom Left Corner
+#define DBRC 188 // ╝, Double Bottom Right Corner
+#define DVSB 186 // ║, Double Vertical Simple Border
+#define DVRB 185 // ╣, Double Vertical Right Border
+#define DVLB 204 // ╠, Double Vertical Left Border
+#define DHSB 205 // ═, Double Horizontal Simple Border
+#define DHBB 202 // ╩, Double Horizontal Bottom Border
+#define DHTB 203 // ╦, Double Horizontal Top Border
+#define DC   206 // ╬, Double Center
 #define MaxGrile 10
 
 void Game(){
-    float grille[MaxGrile][MaxGrile];
+    int grille[MaxGrile][MaxGrile];
 
     for(int i = 0;i < MaxGrile;i++){
         for (int j = 0; j < MaxGrile;j++) {
@@ -16,13 +39,38 @@ void Game(){
     }
     printf("\n\n    A B C D E F G H I J\n");
     for (int i = 0; i < MaxGrile;i++){
+        printf("   ");
+        if(i == 0){
+            printf("%c",STLC);
+        }
+        else{
+            printf("%c",SVLB);
+        }
+        if(i == 0){
+            for (int k = 0; k < MaxGrile*2; ++k) {
+                printf("%c",SHSB);
+            }
+        }
+        else{
+            for (int k = 0; k < MaxGrile; ++k) {
+                printf("%c",SHSB);
+                printf("%c",SHBB);
+            }
+        }
+        if(i == 0){
+            printf("%c",STRC);
+        }
+        else{
+            printf("%c",SVRB);
+        }
         printf("\n%d  ",i+1);
-        if(i < 9){
+        if(i < 9) {
             printf(" ");
         }
         for (int j = 0; j < MaxGrile; ++j) {
-            printf("%.0f ",grille[i][j]);
+            printf("%d ",grille[i][j]);
         }
+        printf("\n");
     }
 
 }
